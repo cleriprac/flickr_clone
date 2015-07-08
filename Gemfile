e41f98fbdc53ed89996fcd7c51767298f1eac61c
cleri philiastides & dan minor <student@epicodus.com>
2015-07-08 11:14:13 -0700

    add paperclip to upload an avator in new registration

---
+++ Gemfile
@@ -0,0 +1,33 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+gem 'pg'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'pry'
+gem 'bootstrap-sass'
+gem 'bcrypt', require: "bcrypt"
+
+group :development do
+  gem 'byebug'
+  gem 'web-console'
+  gem 'spring'
+  gem 'quiet_assets'
+  gem 'letter_opener'
+  gem 'devise'
+  gem "paperclip", "~> 4.3"
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'launchy'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+  gem 'capybara'
+  gem 'factory_girl_rails'
+end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  # attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+  #NOT AVALIBLE IN RAILS 4
+
+  validates_presence_of :username
+
+end
